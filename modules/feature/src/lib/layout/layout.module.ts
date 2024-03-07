a9c9dec956ac054273ea55d4006085de186dc422
Felipe Luna <felipe.luna@totvs.com.br>
2024-03-07 19:25:36 -0300

    feat(header): Header adequacy

--- modules/feature/src/lib/layout/layout.module.ts
+++ modules/feature/src/lib/layout/layout.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
