9a6e8189fa54cbcd6a14322f1be622ba3be0a98b
Rosana Damasceno <rosanaxica7@yahoo.com.br>
2024-03-13 18:50:22 -0300

    chore(userDetail): formating imports

--- modules/data-access/user/src/index.ts
+++ modules/data-access/user/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/services/user-search/user-search.service';
 export * from './lib/mocks/user.mock';
 export * from './lib/models/user.model';
+export * from './lib/services/user-search/user-search.service';
