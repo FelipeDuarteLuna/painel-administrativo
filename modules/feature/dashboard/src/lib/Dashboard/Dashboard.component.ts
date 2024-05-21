382cc6b1cc77abdd281612e5e6188a32ec753ee5
Felipe Luna <felipe.luna@totvs.com.br>
2024-05-21 20:30:18 -0300

    feat(Dashboard): Implementing Reactivity in dataAccess

--- modules/feature/dashboard/src/lib/Dashboard/Dashboard.component.ts
+++ modules/feature/dashboard/src/lib/Dashboard/Dashboard.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { NavigationExtras, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { User, UserSearchService } from 'user-data-access';
 
 @Component({
@@ -13,29 +14,26 @@ import { User, UserSearchService } from 'user-data-access';
   templateUrl: './Dashboard.component.html',
   styleUrl: './Dashboard.component.scss',
 })
-export class DashboardComponent implements AfterViewInit {
+export class DashboardComponent implements OnInit {
   numberUser: number = 0;
   displayedColumns: string[] = ['id', 'avatar', 'name', 'email', 'biography'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  user$!: Observable<User[]>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   router = inject(Router);
   userDataAccess = inject(UserSearchService);
 
-  ngAfterViewInit() {
+  ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.searchUser();
   }
 
   searchUser() {
-    this.userDataAccess.getUsers().subscribe((data: PeriodicElement[]) => {
-      // Atualize ELEMENT_DATA com os dados da API
-      ELEMENT_DATA = data;
-      this.dataSource.data = ELEMENT_DATA;
-
-      // Atualiza o número de usuários com base no tamanho do array retornado pela API
-      this.numberUser = data.length;
+    this.user$ = this.userDataAccess.getUsers();
+    this.user$.subscribe((usersArray) => {
+      this.numberUser = usersArray.length;
     });
   }
 
@@ -59,4 +57,4 @@ export interface PeriodicElement {
   avatar?: string;
 }
 
-let ELEMENT_DATA: PeriodicElement[] = [];
+const ELEMENT_DATA: PeriodicElement[] = [];
